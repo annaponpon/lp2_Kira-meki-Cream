989f041b37eacd773ed2a8f7a82c337c643f5cf8
anna.machida <annamachida0609@gmail.com>
2024-08-17 19:34:35 +0900

    voices

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,19 +1,19 @@
-$('.voices-slick-container').slick({
-    autoplay: true, // 自動再生
+$('.voices__slick').slick({
     dots: true, //ドットのナビ表示
     arrows: true, // 前・次のスライド切り替える矢印表示
-    prevArrow: '<img src="/assets/image/voices_arrow_left.svg" class="slick-arrow prev-arrow">', 
-    nextArrow: '<img src="/assets/image/voices_arrow_right.svg" class="slick-arrow next-arrow">', 
+    prevArrow: '<img src="/assets/image/voices_arrow_left_pc.png" class="slick-arrow prev-arrow">', 
+    nextArrow: '<img src="/assets/image/voices_arrow_right_pc.png" class="slick-arrow next-arrow">', 
     slidesToShow: 3, //表示するスライド枚数
-    slideToScroll: 1, //1回で動くスライド数
-    autoplaySpeed: 3000, // 再生速度（ミリ秒）
+    slidesToScroll: 3, //1回で動くスライド数
     infinite: true, // 無限再生
-    cssEase: 'linear', // アニメーションイージング
     responsive: [{
-      breakpoint: 768, // ブレイクポイントを指定
-      settings: {
-        slidesToShow: 2,
-        },
-      },
+      breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          prevArrow: '<img src="/assets/image/voices_arrow_left_sp.png" class="slick-arrow prev-arrow">', 
+          nextArrow: '<img src="/assets/image/voices_arrow_right_sp.png" class="slick-arrow next-arrow">', 
+      }
+    }
     ]
 });
